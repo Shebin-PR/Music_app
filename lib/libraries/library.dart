1a28b4ac3d09e5f45b57a2a2282fa0aa051b45b0
unknown <shebinpr126@gmail.com>
2021-12-04 15:54:54 +0530

    success

--- lib/libraries/library.dart
+++ lib/libraries/library.dart
@@ -113,7 +113,7 @@ class _LibraryState extends State<Library> {
                                 )),
                             Icon(
                               Icons.favorite,
-                              color: Colors.red,
+                              color: Colors.black,
                             )
                           ],
                         ),
